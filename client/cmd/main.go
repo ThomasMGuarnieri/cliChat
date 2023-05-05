d480ccf56e0c288ee78ffd689e4f5c22a3c3abf5
Thomas M Guanrieri <thomasmguarnieri@gmail.com>
2023-05-04 22:17:19 -0300

    first commit

---
+++ client/cmd/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"simpleChat/client"
+)
+
+func main() {
+	var err error
+
+	addr := flag.String("server", "localhost:3333", "Which server to connect to")
+
+	flag.Parse()
+
+	c := client.NewClient()
+	err = c.Dial(*addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer c.Close()
+
+	go c.Start()
+
+	// Get username
+	fmt.Println("Qual o seu nome meu bom?")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	err = scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.SetName(scanner.Text())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			select {
+			case err := <-c.Error():
+				if err == io.EOF {
+					fmt.Println("Connection closed connection from server.")
+				} else {
+					panic(err)
+				}
+			case msg := <-c.Incoming():
+				fmt.Printf("%v: %v\n", msg.Name, msg.Message)
+			}
+		}
+	}()
+
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		err = scanner.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = c.SendMessage(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
